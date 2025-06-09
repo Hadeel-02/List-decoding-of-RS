feb00cfe6acec7c50ad8a177b749db9856ebc29c
Hadeel-02 <hadeelma.cs@gmail.com>
2025-06-09 11:43:07 +0300

    Update tests.py with 20 new test cases

--- tests.py
+++ tests.py
@@ -1,136 +1,181 @@
 import encoder_decoder
 def test1():
-    print("\n********** Test1 **********")
-    msg = "abc"
+    print("\n********** Test 1 **********")
+    msg = "a"
     k = len(msg)
-    n = 3
+    n = 1 
     e = 0
     eval_pts = [alpha for alpha in range(n)]
     encoder_decoder.run(msg, n, k, e, eval_pts)
 
 def test2():
-    print("\n********** Test2 **********")
+    print("\n********** Test 2 **********")
     msg = "a"
+    k = len(msg)
+    n = 10
+    e = 1
+    eval_pts = [alpha for alpha in range(n)]
+    encoder_decoder.run(msg, n, k, e, eval_pts)
+
+def test3():
+    print("\n********** Test 3 **********")
+    msg = "abc"
+    k = len(msg)
+    n = 8
+    e = 1
+    eval_pts = [alpha for alpha in range(n)]
+    encoder_decoder.run(msg, n, k, e, eval_pts)
+
+def test4():
+    print("\n********** Test 4 **********")
+    msg = "Hello"
+    k = len(msg)
+    n = 10
+    e = 2
+    eval_pts = [alpha for alpha in range(n)]
+    encoder_decoder.run(msg, n, k, e, eval_pts)
+
+def test5():
+    print("\n********** Test 5 **********")
+    msg = "World"
+    k = len(msg)
+    n = 15
+    e = 3
+    eval_pts = [alpha for alpha in range(n)]
+    encoder_decoder.run(msg, n, k, e, eval_pts)
+
+def test6():
+    print("\n********** Test 6 **********")
+    msg = "testing"
+    k = len(msg)
+    n = 7
+    e = 6
+    eval_pts = [alpha for alpha in range(n)]
+    encoder_decoder.run(msg, n, k, e, eval_pts)
+
+def test7():
+    print("\n********** Test 7 **********")
+    msg = "testing"
+    k = len(msg)
+    n = 30
+    e = 6
+    eval_pts = [alpha for alpha in range(n)]
+    encoder_decoder.run(msg, n, k, e, eval_pts)
+
+def test8():
+    print("\n********** Test 8 **********")
+    msg = "Python"
+    k = len(msg)
+    n = 15
+    e = 3
+    eval_pts = [alpha for alpha in range(n)]
+    encoder_decoder.run(msg, n, k, e, eval_pts)
+
+def test9():
+    print("\n********** Test 9 **********")
+    msg = "PROJECT"
+    k = len(msg)
+    n = 25
+    e = 4
+    eval_pts = [alpha for alpha in range(n)]
+    encoder_decoder.run(msg, n, k, e, eval_pts)
+
+def test10():
+    print("\n********** Test 10 **********")
+    msg = "example"
+    k = len(msg)
+    n = 18
+    e = 3
+    eval_pts = [alpha for alpha in range(n)]
+    encoder_decoder.run(msg, n, k, e, eval_pts)
+
+def test11():
+    print("\n********** Test 11 **********")
+    msg = "decode"
+    k = len(msg)
+    n = 14
+    e = 2
+    eval_pts = [alpha for alpha in range(n)]
+    encoder_decoder.run(msg, n, k, e, eval_pts)
+
+def test12():
+    print("\n********** Test 12 **********")
+    msg = "List decoding"
+    k = len(msg)
+    n = 26
+    e = 0
+    eval_pts = [alpha for alpha in range(n)]
+    encoder_decoder.run(msg, n, k, e, eval_pts)
+
+def test13():
+    print("\n********** Test 13 **********")
+    msg = "message"
+    k = len(msg)
+    n = 35
+    e = 7
+    eval_pts = [alpha for alpha in range(n)]
+    encoder_decoder.run(msg, n, k, e, eval_pts)
+
+def test14():
+    print("\n********** Test 14 **********")
+    msg = "Computer Science"
+    k = len(msg)
+    n = 30
+    e = 1
+    eval_pts = [alpha for alpha in range(n)]
+    encoder_decoder.run(msg, n, k, e, eval_pts)
+
+def test15():
+    print("\n********** Test 15 **********")
+    msg = "Computer Science"
+    k = len(msg)
+    n = 32
+    e = 1
+    eval_pts = [alpha for alpha in range(n)]
+    encoder_decoder.run(msg, n, k, e, eval_pts)
+
+def test16():
+    print("\n********** Test 16 **********")
+    msg = "codes"
+    k = len(msg)
+    n = 40
+    e = 5
+    eval_pts = [alpha for alpha in range(n)]
+    encoder_decoder.run(msg, n, k, e, eval_pts)
+
+def test17():
+    print("\n********** Test 17 **********")
+    msg = "Reed Solomon"
+    k = len(msg)
+    n = 60
+    e = 6
+    eval_pts = [alpha for alpha in range(n)]
+    encoder_decoder.run(msg, n, k, e, eval_pts)
+
+def test18():
+    print("\n********** Test 18 **********")
+    msg = "Computer Science"
     k = len(msg)
     n = 2
     e = 0
     eval_pts = [alpha for alpha in range(n)]
     encoder_decoder.run(msg, n, k, e, eval_pts)
 
-def test3():
-    print("\n********** Test3 **********")
-    msg = "a"
-    k = len(msg)
-    n = 10
-    e = 0
-    eval_pts = [alpha for alpha in range(n)]
-    encoder_decoder.run(msg, n, k, e, eval_pts)
-
-def test4():
-    print("\n********** Test4 **********")
-    msg = "Hello"
-    k = len(msg)
-    n = 15
-    e = 2
-    eval_pts = [alpha for alpha in range(n)]
-    encoder_decoder.run(msg, n, k, e, eval_pts)
-
-def test5():
-    print("\n********** Test5 **********")
-    msg = "world"
-    k = len(msg)
-    n = 10
-    e = 3
-    eval_pts = [alpha for alpha in range(n)]
-    encoder_decoder.run(msg, n, k, e, eval_pts)
-
-def test6():
-    print("\n********** Test6 **********")
-    msg = "testing"
-    k = len(msg)
-    n = 15
-    e = 2
-    eval_pts = [alpha for alpha in range(n)]
-    encoder_decoder.run(msg, n, k, e, eval_pts)
-
-def test7():
-    print("\n********** Test7 **********")
-    msg = "testing"
-    k = len(msg)
-    n = 20
-    e = 6
-    eval_pts = [alpha for alpha in range(n)]
-    encoder_decoder.run(msg, n, k, e, eval_pts)
-
-def test8():
-    print("\n********** Test8 **********")
-    msg = "abc123"
+def test19():
+    print("\n********** Test 19 **********")
+    msg = "Bye!"
     k = len(msg)
     n = 8
     e = 0
     eval_pts = [alpha for alpha in range(n)]
     encoder_decoder.run(msg, n, k, e, eval_pts)
 
-def test9():
-    print("\n********** Test9 **********")
-    msg = "python"
-    k = len(msg)
-    n = 30
-    e = 5
-    eval_pts = [alpha for alpha in range(n)]
-    encoder_decoder.run(msg, n, k, e, eval_pts)
-
-def test10():
-    print("\n********** Test10 **********")
-    msg = "PROJECT"
-    k = len(msg)
-    n = 30
-    e = 2
-    eval_pts = [alpha for alpha in range(n)]
-    encoder_decoder.run(msg, n, k, e, eval_pts)
-
-def test11():
-    print("\n********** Test11 **********")
-    msg = "example"
-    k = len(msg)
-    n = 9
-    e = 3
-    eval_pts = [alpha for alpha in range(n)]
-    encoder_decoder.run(msg, n, k, e, eval_pts)
-
-def test12():
-    print("\n********** Test12 **********")
-    msg = "decode"
-    k = len(msg)
-    n = 7
-    e = 1
-    eval_pts = [alpha for alpha in range(n)]
-    encoder_decoder.run(msg, n, k, e, eval_pts)
-
-def test13():
-    print("\n********** Test13 **********")
-    msg = "cipher"
-    k = len(msg)
-    n = 6
-    e = 2
-    eval_pts = [alpha for alpha in range(n)]
-    encoder_decoder.run(msg, n, k, e, eval_pts)
-
-def test14():
-    print("\n********** Test14 **********")
-    msg = "encryption"
-    k = len(msg)
-    n = 11
-    e = 4
-    eval_pts = [alpha for alpha in range(n)]
-    encoder_decoder.run(msg, n, k, e, eval_pts)
-
-def test15():
-    print("\n********** Test15 **********")
-    msg = "security"
+def test20():
+    print("\n********** Test 20 **********")
+    msg = "Bye!"
     k = len(msg)
     n = 8
-    e = 3
+    e = 2
     eval_pts = [alpha for alpha in range(n)]
     encoder_decoder.run(msg, n, k, e, eval_pts)
 
@@ -151,17 +196,22 @@ def your_test():
 
 if __name__ == "__main__":
     test1()
-    # test2()
-    # test3()
-    # test4()
-    # test5()
-    # test6()
-    # test7()
-    # test8()
-    # test9()
-    # test10()
-    # test11()
-    # test12()
-    # test13()
-    # test14()
-    # test15()
+    test2()
+    test3()
+    test4()
+    test5()
+    test6()
+    test7()
+    test8()
+    test9()
+    test10()
+    test11()
+    test12()
+    test13()
+    test14()
+    test15()
+    test16()
+    test17()
+    test18()
+    test19()
+    test20()
